46893361091454bc5fae5d5c1d0793a560bbd8c6
yiğithan <yigithangediz@gmail.com>
2021-05-10 10:55:52 +0300

    Lane waypoints are drawn to the middle of the x axis and slope is used to determine the direction

--- dev_ws/src/install/lane_detector/lib/python3.6/site-packages/lane_detector/LeftWaypointsNode.py
+++ dev_ws/src/install/lane_detector/lib/python3.6/site-packages/lane_detector/LeftWaypointsNode.py
@@ -29,29 +29,33 @@ class LeftWaypointsNode(Node):
         )
 
 
-    def callback(self, msg,n_samples = 100):
+    def callback(self, msg,n_samples = 100, x_max = 1920, y_max=1080):
         slope = msg.slope
         bias = msg.bias
 
         t_slope, t_bias = self.transform(slope, bias)
 
         if t_slope is not np.inf:
-            line = np.poly1d([t_slope, t_bias])
+            # Ortaya göre yapalım önce.
+            start_x = x_max/2
+            start_y = y_max -1
 
-            # Implement the line near the middle of the x axis
-            x_space = np.linspace(540-(n_samples/2),540+(n_samples/2) ,n_samples + 1, dtype="float64")
+            x_space = np.linspace(start_x - n_samples/2, start_x + n_samples/2, n_samples +1, dtype="float64")
+            line_coeffs = np.array([t_slope, start_y - t_slope*start_x])
+            line = np.poly1d(line_coeffs)
             y_space = line(x_space)
 
-            data = WaypointData()
+            # Coefficients of the line found and whether it is vertical or rotated
             data2 = LaneCoeffs()
+            data2.name = "Rotated"
+            data2.slope = line_coeffs[0]
+            data2.bias = line_coeffs[1]
 
+            # Waypoint data
+            data = WaypointData()
             data.is_inf = False
             data.x = list(x_space)
             data.y = list(y_space)
-
-            data2.name = "Rotated"
-            data2.slope = t_slope
-            data2.bias = t_bias
 
             self.publisher2.publish(data2)
             self.publisher.publish(data)
@@ -61,26 +65,25 @@ class LeftWaypointsNode(Node):
             data2 = LaneCoeffs()
 
             data.is_inf = True
-            data.x = [t_bias+200]*n_samples
-            data.y = list(np.linspace(1920-n_samples, 1920, n_samples+1).astype("float64"))
+            data.x = [x_max/2]*n_samples
+            data.y = list(np.linspace(y_max, y_max-n_samples-1, n_samples, dtype="float64"))
 
             data2.name = "Vertical"
             data2.slope = 999999999999999.
-            data2.bias = t_bias
+            data2.bias = x_max/2
 
             self.publisher2.publish(data2)
             self.publisher.publish(data)
 
 
     def transform(self, slope, bias, vertical_slope_thresh=20):
-        # Rotate the line 90 degrees clockwise
+        # Rotate the line 90 degrees
         rot_slope = (1/slope)
-        rot_bias = (-1*bias/slope)
+        rot_bias = bias
 
         # If slope's absolute value is greater than threshold, the line is vertical
         if abs(rot_slope) >= vertical_slope_thresh:
             rot_slope = np.inf
-            rot_bias = bias
 
         return (rot_slope, rot_bias)
 
