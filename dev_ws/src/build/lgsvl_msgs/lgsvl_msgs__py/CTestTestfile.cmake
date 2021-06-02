2fc1f9c781ad0c7504ac48908a3b00a33774141f
Yigithan Gediz <yigithangediz@gmail.com>
2021-06-02 13:34:51 +0300

    gitignore added

--- dev_ws/src/build/lgsvl_msgs/lgsvl_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yigithan/Desktop/PID_lane_follower/dev_ws/src/build/lgsvl_msgs/lgsvl_msgs__py
-# Build directory: /home/yigithan/Desktop/PID_lane_follower/dev_ws/src/build/lgsvl_msgs/lgsvl_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- dev_ws/src/build/spark_msgs/CMakeFiles/spark_msgs.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/spark_msgs"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/spark_msgs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
